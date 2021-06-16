692bd9dbf441bc41fd2a25a89ee20bd12fa6773b
Jesus Hernandez <jesus@jesushf.dev>
2021-06-16 16:02:52 -0700

    Code + Project Changes
    Pushing some semi working code and mode change from 2D to 3D

---
+++ BWGameJam2021/Assets/Scripts/Collectible.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Collectible : MonoBehaviour
+{
+    private float radius;
+    [SerializeField] float radiusSpeed, theta, thetaSpeed;
+
+    [SerializeField] private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = Vector2.Distance(this.gameObject.transform.position, player.transform.position);
+        radiusSpeed = 1;
+        thetaSpeed = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AbsorbedByPlayer();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            this.gameObject.SetActive(false);
+    }
+
+    private void AbsorbedByPlayer()
+    {
+        float t = 10 - ((Time.time * 2) % 10);
+
+        theta = t * thetaSpeed;
+        radius = t * radiusSpeed;
+
+        transform.position = new Vector2(radius * Mathf.Cos(Mathf.Deg2Rad * theta) + player.transform.position.x, radius * Mathf.Sin(Mathf.Deg2Rad * theta) + player.transform.position.y);
+    }
+}
---
+++ BWGameJam2021/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    enum PlayerState
+    {
+        Normal = 0, TimeStop, Growing
+    }
+
+    PlayerState ps = PlayerState.Normal;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        ps = PlayerState.Normal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && ps != PlayerState.TimeStop)
+        {
+            LightSpeed();
+            ps = PlayerState.Normal;
+        }
+    }
+
+    void LightSpeed()
+    {
+        Time.timeScale = 0f;
+        ps = PlayerState.TimeStop;
+        this.transform.position += new Vector3(20f, 0, 0) * Time.unscaledDeltaTime;
+        Time.timeScale = 1f;
+    }
+
+    IEnumerator SlowPlayerDown()
+    {
+        while (rb.velocity.x > 10)
+        {
+            rb.velocity = rb.velocity * .5f * Time.unscaledDeltaTime;
+            yield return new WaitForSecondsRealtime(5);
+        }
+    }
+}
