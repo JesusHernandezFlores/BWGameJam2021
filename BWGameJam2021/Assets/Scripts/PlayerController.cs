fb3ffe462190e93ad55ba6e6dac8f9a91a179763
Jesus Hernandez <jesus@jesushf.dev>
2021-06-16 18:05:55 -0700

    Fixes to last commit

--- BWGameJam2021/Assets/Scripts/PlayerController.cs
+++ BWGameJam2021/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    enum PlayerState
-    {
-        Normal = 0, TimeStop, Growing
-    }
-
     [SerializeField] float movementSpeed = 10;
-    PlayerState ps = PlayerState.Normal;
     Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-        ps = PlayerState.Normal;
     }
 
     // Update is called once per frame
@@ -45,7 +37,7 @@ public class PlayerController : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(-transform.forward * movementSpeed * Time.unscaledDeltaTime);
+            transform.Translate(-transform.right * movementSpeed * Time.unscaledDeltaTime);
         }
 
         if (Input.GetKey(KeyCode.C))
